a60b0d7f5f558295d9237fceb8cb9719a8ac7335
BenMullan <BenMullan@outlook.de>
2024-06-08 21:46:12 +0100

    .

---
+++ make-readme.ps1
@@ -0,0 +1,15 @@
+# File:		make-readme.ps1 - generates readme.md, from all image files in current directory's direct subfolders
+# Exec:		pwsh.exe -File make-readme.ps1
+# Author:	Ben Mullan (c) 2024
+
+@"
+# Witty Marketing Posters
+BM's utterly hysterical, sometimes casuistic, & unpardonably witty marketing posters
+$(
+	dir | ? psIsContainer | % {
+		$_.getFiles("*.png") | % {
+			"`n<img src=""https://github.com/BenMullan/notebook-2-app/blob/main/$($_.directory.name)/$($_.name)?raw=true"" width=""100%"" />"
+		}
+	}
+)
+"@ | out-file -filePath "./readme.md"
